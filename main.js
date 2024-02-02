b42058a40369d6508bbe2a60ccdc789308f4f97e
yanaksalvo <142211410+yanaksalvo@users.noreply.github.com>
2024-02-02 20:15:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// Node.js'de çalıştırmak için ihtiyacınız olan modülü alın. Discord.js sürüm 12.3.1'i kullanıyoruz.
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({ restRequestTimeout: 60000, checkUpdate: false });
 
@@ -7,22 +6,21 @@ client.on("ready", async () => {
     console.log("Olarak giriş yapıldı " + client.user.tag);
 });
 
-// Mesajları dinle
 client.on("message", (msg) => {
     handleMessage(msg);
 });
 
-// Mesaj güncellemelerini dinle
+
 client.on("messageUpdate", (oldMessage, newMessage) => {
-    handleMessage(newMessage, true); // messageUpdate olayında true parametresini geçin
+    handleMessage(newMessage, true); 
 });
 
 function handleMessage(msg, isUpdated = false) {
     if (
         msg.author.id === "689766089567109158" && 
-        !msg.content.includes("http") && // Eğer mesaj bir bağlantı içermiyorsa
-        msg.content !== "" && // Eğer mesaj boş değilse
-        !msg.embeds[0] && // Eğer mesaj bir gömme içermiyorsa
+        !msg.content.includes("http") && 
+        msg.content !== "" && 
+        !msg.embeds[0] && 
         msg.author.id !== "1177934461527146576"
     ) {
         if (isUpdated) {
@@ -40,11 +38,11 @@ function handleMessage(msg, isUpdated = false) {
                 msg.channel.send(kelime).catch((error) => {
                     console.log(error);
                 });
-            }, 50); // 50 milisaniye (0.050 saniye) bekletme süresi
+            }, 50); 
         }
     } else if (msg.author.id === "BOT_ID") {
         return;
     }
 }
 
-client.login("ODA5MDQ2MTk0MTg5MzY5MzU1.G0lpZl.3W95zNpSkyM4KVKZnBCghu47LQjP26iYDbhxUI");
+client.login("");
